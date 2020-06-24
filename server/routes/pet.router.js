2f9f04f044ef1fa00b783b078359c121f84a3465
Steve Simonson <srsimonson@gmail.com>
2020-06-24 15:33:34 -0500

    auth set up

--- server/routes/pet.router.js
+++ server/routes/pet.router.js
@@ -4,16 +4,20 @@ const pool = require('../modules/pool');
 
 // This route *should* return the logged in users pets
 router.get('/', (req, res) => {
+    if(req.isAuthenticated() && req.user.security === 'admin'){
     console.log('/pet GET route');
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
-    let queryText = `SELECT * FROM "pet"`;
+    let queryText = `SELECT * FROM pet WHERE user_id=${req.user.id}`;
     pool.query(queryText).then((result) => {
         res.send(result.rows);
     }).catch((error) => {
         console.log(error);
         res.sendStatus(500);
     });
+} else {
+    res.sendStatus(401);
+}
 });
 
 // This route *should* add a pet for the logged in user
